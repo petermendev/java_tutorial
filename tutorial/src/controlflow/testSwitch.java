a4c0062d7862e1c2b3908dd30857d6631a142e63
admin <admin@DESKTOP-ETTE3A8>
2019-11-16 19:58:49 +0200

    new package

---
+++ tutorial/src/controlflow/testSwitch.java
@@ -0,0 +1,27 @@
+package controlflow;
+
+public class testSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=2;
+		
+		switch (x) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		case 3:case 4:case 5:
+			System.out.println("3 or more");
+			break;
+		default:
+			System.out.println("else");
+			break;
+		}
+		
+	}
+
+}
